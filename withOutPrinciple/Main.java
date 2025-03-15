66f312e42741a24a02c4bbd61756db57abe9e6b5
Fahad15983 <fahadahmedshah13@gmail.com>
2025-03-16 01:57:55 +0500

    Add files via upload

---
+++ withOutPrinciple/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package withOutPrinciple;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+        circle.drawCircle();
+        rectangle.drawRectangle();
+    }
+}
+
